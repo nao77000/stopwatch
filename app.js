86259c2ae6cb64b98587eb0e859cdbd65929cf65
nao77000 <145801150+nao77000@users.noreply.github.com>
2023-10-04 23:38:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let timeoutID;
 
 function displayTime() {
   const currentTime = new Date(Date.now() - startTime + stopTime);
-  const h = String(currentTime.getHours()-1).padStart(2, '0');
+  const h = String(currentTime.getHours()-9).padStart(2, '0');
   const m = String(currentTime.getMinutes()).padStart(2, '0');
   const s = String(currentTime.getSeconds()).padStart(2, '0');
   const ms = String(currentTime.getMilliseconds()).padStart(3, '0');
